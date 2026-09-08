14646880df98e5b0ad8bbfb9376a76804944ae01
tomoaki <output@unwraps.net>
2026-09-08 12:58:48 +0900

    Initial Commit

---
+++ Shift Upload/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shift Upload
+//
+//  Created by tomoaki-d on 2026/09/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shift Upload/Shift_UploadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shift_UploadApp.swift
+//  Shift Upload
+//
+//  Created by tomoaki-d on 2026/09/08.
+//
+
+import SwiftUI
+
+@main
+struct Shift_UploadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
